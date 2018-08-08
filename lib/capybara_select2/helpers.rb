2686e82224c6fdecbd395b73703edc54a7b6502b
Dzmitry <dzmitry.kovalenok@thehamon.com>
2018-08-08 19:41:58 +0300

    Extract selectors into variables

--- lib/capybara_select2/helpers.rb
+++ lib/capybara_select2/helpers.rb
@@ -1,6 +1,22 @@
 module CapybaraSelect2
   module Helpers
     def select2(value, options = {})
+      open_select = "" +
+        ".select2-choice, .select2-choices, " +                    # v2 & v3
+        ".select2-selection"                                       # v4
+
+      search_input = "" +
+        ".select2-dropdown-open input.select2-focused,"  +         # v2
+        ".select2-drop-active input.select2-input,"      +         # v3
+        ".select2-container--focus input.select2-search__field," + # v4 single
+        ".select2-container--open input.select2-search__field"     # v4 multi
+
+      option = "" +
+        ".select2-container-active .select2-highlighted," +        # v2
+        ".select2-drop-active .select2-highlighted,"      +        # v3
+        ".select2-results .select2-results__option--highlighted"   # v4
+
+
       container = if options[:xpath]
         find(:xpath, options[:xpath])
       elsif options[:css]
@@ -12,25 +28,11 @@ module CapybaraSelect2
         find(:css, "#s2id_#{select_id}")
       end
 
-      container
-        .find(
-          ".select2-choice, .select2-choices, " + # v2 & v3
-          ".select2-selection"                    # v4
-        ).click
+      container.find(open_select).click
 
-      find(
-        ".select2-dropdown-open input.select2-focused,"  +         # v2
-        ".select2-drop-active input.select2-input,"      +         # v3
-        ".select2-container--focus input.select2-search__field," + # v4 single
-        ".select2-container--open input.select2-search__field"     # v4 multi
-      ).set value
+      find(search_input).set value
 
-      find(
-        ".select2-container-active .select2-highlighted," +        # v2
-        ".select2-drop-active .select2-highlighted,"      +        # v3
-        ".select2-results .select2-results__option--highlighted",  # v4
-        text: value
-      ).click
+      find(option, text: value).click
     end
 
   end
