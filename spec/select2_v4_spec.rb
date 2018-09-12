feb9ed8a1850eabb34f8c25826a60252741466b4
Dzmitry <dzmitry.kovalenok@thehamon.com>
2018-09-12 20:46:26 +0300

    Improve wording in rspec

--- spec/select2_v4_spec.rb
+++ spec/select2_v4_spec.rb
@@ -11,17 +11,17 @@ describe CapybaraSelect2 do
 
       context 'selecting a value from a single select box' do
 
-        it 'identified by XPath' do
+        it 'should select an option from select found by XPath' do
           select2 'XBox', xpath: "//div[@id='single']"
           expect(page).to have_css '.select2-container span', text: 'XBox'
         end
 
-        it 'identified by CSS selector' do
+        it 'should select an option from select found by CSS selector' do
           select2 'XBox', css: '#single .select2-container'
           expect(page).to have_css '.select2-container span', text: 'XBox'
         end
 
-        it 'identified by label' do
+        it 'should select an option from select found by label' do
           select2 'XBox', from: 'Select game console'
           expect(page).to have_css '.select2-container span', text: 'XBox'
         end
@@ -30,17 +30,17 @@ describe CapybaraSelect2 do
 
       context 'selecting a value from a multi select box' do
 
-        it 'identified by XPath' do
+        it 'should select an option from select found by XPath' do
           select2 'Buy Milk', xpath: "//div[@id='multiple']"
           expect(page).to have_css '.select2-selection__choice', text: 'Buy Milk'
         end
 
-        it 'identified by CSS selector' do
+        it 'should select an option from select found by CSS selector' do
           select2 'Buy Milk', css: '#multiple .select2-container'
           expect(page).to have_css '.select2-selection__choice', text: 'Buy Milk'
         end
 
-        it 'identified by label' do
+        it 'should select an option from select found by label' do
           select2 'Buy Milk', from: 'Things to do'
           expect(page).to have_css '.select2-selection__choice', text: 'Buy Milk'
         end
